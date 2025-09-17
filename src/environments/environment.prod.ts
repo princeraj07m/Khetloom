df073b6468eda383601c47db8950def1d0f1bd7d
Prince Kumar <princerajm31@email.com>
2025-09-18 02:58:13 +0530

    dash update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,14 +2,14 @@ export const environment = {
   production: true,
 
   // aws url 
-  // apiUrl: 'http://13.60.157.181:5001/api',
-  // backendUrl: 'http://13.60.157.181:5001',
-  // weatherApiKey: 'b6907d289e10d714a6e88b30761fae22',
-  // weatherApiUrl: 'https://api.openweathermap.org/data/2.5'
-
-  // vercel url 
-  apiUrl: 'https://khetloom-backend.vercel.app/api',
-  backendUrl: 'https://khetloom-backend.vercel.app',
+  apiUrl: 'http://13.60.157.181:5001/api',
+  backendUrl: 'http://13.60.157.181:5001',
   weatherApiKey: 'b6907d289e10d714a6e88b30761fae22',
   weatherApiUrl: 'https://api.openweathermap.org/data/2.5'
+
+  // vercel url 
+  // apiUrl: 'https://khetloom-backend.vercel.app/api',
+  // backendUrl: 'https://khetloom-backend.vercel.app',
+  // weatherApiKey: 'b6907d289e10d714a6e88b30761fae22',
+  // weatherApiUrl: 'https://api.openweathermap.org/data/2.5'
 };
